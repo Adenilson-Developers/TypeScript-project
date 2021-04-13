5754e733e12aec6447de0a3493bd01e7fc201e7e
Adenilson-Developers <adenilson.developers@gmail.com>
2021-04-12 23:28:06 -0300

    add configuration typescripet

---
+++ app/ts/models/negociacao.ts
@@ -0,0 +1,34 @@
+class Negociacao {
+
+   private _data;
+   private _quantidade;
+   private _valor;
+    
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+    }
+// Regras: uma negociação depois que criado não pode ter suas propriedades alteradas
+// o "_" indica uma conversão no javascript que diz que as essas propriedades não pode ser alterada
+// fora dos métodos da propria classe
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    // Esses methodos que permite acessar os valores.
+}
