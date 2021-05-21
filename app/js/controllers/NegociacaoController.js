5aa68a528bf508ae18f95c5bae484ee5f2cc4391
Adenilson-Developers <adenilson.developers@gmail.com>
2021-05-21 15:57:00 -0300

    add system para carregar os modulos

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,23 +1,47 @@
-class NegociacaoControler {
-    constructor() {
-        this._negociacoes = new Negociacoes();
-        this._negociacoesView = new NegociacoesView('#negociacoesView');
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-        this._negociacoesView.update(this._negociacoes);
-    }
-    adicionar(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
-        this._negociacoes.adicionar(negociacao);
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso!');
-        this._negociacoes.paraArray().forEach(negociacao => {
-            console.log(negociacao.data);
-            console.log(negociacao.quantidade);
-            console.log(negociacao.valor);
-        });
-    }
-}
+System.register(["../views/NegociacoesView", "../models/Negociacoes", "../views/MensagemView", "../models/negociacao"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var NegociacoesView_1, Negociacoes_1, MensagemView_1, negociacao_1, NegociacaoController;
+    return {
+        setters: [
+            function (NegociacoesView_1_1) {
+                NegociacoesView_1 = NegociacoesView_1_1;
+            },
+            function (Negociacoes_1_1) {
+                Negociacoes_1 = Negociacoes_1_1;
+            },
+            function (MensagemView_1_1) {
+                MensagemView_1 = MensagemView_1_1;
+            },
+            function (negociacao_1_1) {
+                negociacao_1 = negociacao_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._negociacoes = new Negociacoes_1.Negociacoes();
+                    this._negociacoesView = new NegociacoesView_1.NegociacoesView('#negociacoesView');
+                    this._mensagemView = new MensagemView_1.MensagemView('#mensagemView');
+                    this._inputData = $('#data');
+                    this._inputQuantidade = $('#quantidade');
+                    this._inputValor = $('#valor');
+                    this._negociacoesView.update(this._negociacoes);
+                }
+                adicionar(event) {
+                    event.preventDefault();
+                    const negociacao = new negociacao_1.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    this._negociacoes.adicionar(negociacao);
+                    this._negociacoesView.update(this._negociacoes);
+                    this._mensagemView.update('Negociação adicionada com sucesso!');
+                    this._negociacoes.paraArray().forEach(negociacao => {
+                        console.log(negociacao.data);
+                        console.log(negociacao.quantidade);
+                        console.log(negociacao.valor);
+                    });
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
